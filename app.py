c6ef2444ab3b9cd35797bd943442fcebdcd32ee8
rimeldaa15 <139239615+rimeldaa15@users.noreply.github.com>
2025-02-19 21:01:11 +0700

    app.py

--- app.py
+++ app.py
@@ -18,14 +18,14 @@ with open('model_prediksi.pkl', 'rb') as file:
 st.title("Prediksi Hasil Produksi Padi Provinsi Jawa Tengah")  # Mengganti judul
 
 # Informasi Akurasi Model
-st.markdown("**Berisi 3 Variabel yaitu Luas Tanam (Ha), Luas Panen (Ha), dan Produktivitas (Ku/Ha).**")
+st.markdown("**Berisi 3 Variabel yaitu Luas Tanam (Hektare), Luas Panen (Hektare), dan Produktivitas (Kuintal/Hektare).**")
 
 # Luas Tanam dengan batasan minimal dan maksimal, dan satuan hektar
 Luas_Tanam = st.number_input("Luas Tanam (Ha) (min: 0, maks: 1000000) ", min_value=0.0, max_value=1000000.0, format="%.2f")
 # Luas Panen dengan satuan hekta
 Luas_Panen = st.number_input("Luas Panen (Ha) (min: 0, maks: 1000000) ", min_value=0.0, max_value=1000000.0, format="%.2f")
 # Produktivitas dengan satuan Ku/Ha
-Produktivitas = st.number_input("Produktivitas (Ku/Ha) (min: 0, maks: 100) ", min_value=0.0, max_value=10000.0, format="%.2f")
+Produktivitas = st.number_input("Produktivitas (Ku/Ha) (min: 0, maks: 10000) ", min_value=0.0, max_value=10000.0, format="%.2f")
 
 if st.button("Prediksi"):
     features = np.array([[Luas_Tanam, Luas_Panen, Produktivitas]])
