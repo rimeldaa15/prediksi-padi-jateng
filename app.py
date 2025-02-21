225fade28db5b0f6d85dec5fe22fd9e6e15fcbdb
rimeldaa15 <139239615+rimeldaa15@users.noreply.github.com>
2025-02-21 07:07:04 +0700

    app.py

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ st.title("Prediksi Hasil Produksi Padi Provinsi Jawa Tengah")  # Mengganti judul
 st.markdown("**Terdiri dari 3 Variabel yaitu Luas Tanam (Hektare), Luas Panen (Hektare), dan Produktivitas (Kuintal/Hektare).**")
 
 # Luas Tanam dengan batasan minimal dan maksimal, dan satuan hektar
-Luas_Tanam = st.number_input("Luas Tanam (Ha) (min: 0, maks: 1000000) ", min_value=0.0, max_value=1000000, format="%.2f")
+Luas_Tanam = st.number_input("Luas Tanam (Ha) (min: 0, maks: 1000000) ", min_value=0.0, max_value=1000000.0, format="%.2f")
 # Luas Panen dengan satuan hekta
-Luas_Panen = st.number_input("Luas Panen (Ha) (min: 0, maks: 1000000) ", min_value=0.0, max_value=1000000, format="%.2f")
+Luas_Panen = st.number_input("Luas Panen (Ha) (min: 0, maks: 1000000) ", min_value=0.0, max_value=1000000.0, format="%.2f")
 # Produktivitas dengan satuan Ku/Ha
-Produktivitas = st.number_input("Produktivitas (Ku/Ha) (min: 0, maks: 10000) ", min_value=0.0, max_value=10000, format="%.2f")
+Produktivitas = st.number_input("Produktivitas (Ku/Ha) (min: 0, maks: 10000) ", min_value=0.0, max_value=10000.0, format="%.2f")
 
 if st.button("Prediksi"):
     features = np.array([[Luas_Tanam, Luas_Panen, Produktivitas]])
