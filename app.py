25216e52a7b2577e0fffc5789d7c225086e071f0
rimeldaa15 <139239615+rimeldaa15@users.noreply.github.com>
2025-02-26 07:02:30 +0700

    app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ Luas_Tanam = st.number_input("Luas Tanam (Ha) (min: 0, maks: 1000000) ", min_val
 # Luas Panen dengan satuan hekta
 Luas_Panen = st.number_input("Luas Panen (Ha) (min: 0, maks: 1000000) ", min_value=0.0, max_value=1000000.0, format="%.2f")
 # Produktivitas dengan satuan Ku/Ha
-Produktivitas = st.number_input("Produktivitas (Ku/Ha) (min: 0, maks: 10000) ", min_value=0.0, max_value=10000.0, format="%.2f")
+Produktivitas = st.number_input("Produktivitas (Ku/Ha) (min: 0, maks: 100) ", min_value=0.0, max_value=100.0, format="%.2f")
 
 if st.button("Prediksi"):
     features = np.array([[Luas_Tanam, Luas_Panen, Produktivitas]])
